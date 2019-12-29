b089866094ecc3caedc9688a26029a2394102315
Stefan <stefi5miljevic@gmail.com>
2019-12-29 23:54:47 +0100

    Split header and footer files

---
+++ app/pages/new_ticket.php
@@ -0,0 +1,25 @@
+<?php
+include "includes/header.php";
+?>
+
+<div id="content">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+
+            <button type="button" id="sidebarCollapse" class="btn btn-light">
+                <i class="fas fa-bars"></i>
+            </button>
+
+            <h3>Create New Ticket</h3>
+
+        </div>
+    </nav>
+
+    <!--Page Content-->
+
+
+</div>
+
+<?php
+include "includes/footer.php";
+?>
