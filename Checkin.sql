b28f269360d8d35342e3670a199470ecd051ceac
alinaPolj <aliiina96@live.de>
2020-01-05 16:33:58 +0100

    new checkin precedures + new views

--- Checkin.sql
+++ Checkin.sql
@@ -1,5 +1,3 @@
----------CHECKIN Prozedur begonnen----Nicht fertig!! 
-
 SET serveroutput ON;
 /
 /*********************************************************************
@@ -178,6 +176,7 @@ BEGIN
 					INSERT INTO zimmer_rechnung (zimmernummer, rechnungID) VALUES (l_i_zimmernummer,l_i_rechnungID);
 					-- create entry in rechnung_gaeste
 					INSERT INTO rechnung_gaeste (rechnungID, personID) VALUES (l_i_rechnungID, l_i_personID );
+					COMMIT;
 				ELSE
 					-- check if form exists
 					SELECT count(pensionsform_tageskarteID) INTO l_i_help FROM pensionsform_tageskarte WHERE bezeichnung = l_v_bezeichnung_form_in;
@@ -246,16 +245,380 @@ EXCEPTION
 			raise_application_error(errno,errmsg);
 END;
 /
-/*****************************************************************************************
-select * from person;
-select * from zimmerkategorie;
-select * from pensionsform_tageskarte;
-select * from rechnung;
+
+--EXEC sp_checkin ('Franz', 'Wadsack','2020-01-22', 3, 'Suite','Vollpension ab 12');
+
+
+/*********************************************************************
+/**
+/** Procedure sp_checkin_add_
+/** Out: 
+/** In: l_v_nachname_in - Nachname des Gastes.
+/** In: l_v_vornamen_in - Vorname des Gastes.
+/** In: l_d_zeitstempel_in - Ankunftdatum des Gastes.'YYYY-MM-DD'
+/** In: l_v_vorname_primary_in - Vorname zu dem der Gast hinzugebucht wird
+/** In: l_v_nachname_primary_in - Nachname zu dem der Gast hinzugebucht wird
+/** Developer: Alina Poljanc
+/** Description: The procedure creates a guest, if it's not existent yet,
+/**   					 adds him to a receipt and a room for the given time a the primary guest
+/** 						 In case the given name is not found in the person table,
+/** 						 there is no bed left in  the room or the person has
+/**							 already checked in at another room in the same time, an
+/**							 exception is thrown. 
+/*********************************************************************/
+
+
+CREATE OR REPLACE PROCEDURE sp_checkin_add
+(l_v_vorname_in IN VARCHAR, l_v_nachname_in IN VARCHAR, l_l_d_zeitstempel_in IN VARCHAR, l_v_vorname_primary_in IN VARCHAR, l_v_nachname_primary_in IN VARCHAR)
+AS
+-- Cursor for all rooms of an invoice guest
+	CURSOR l_rooms_for_invoice_cur
+		IS
+			SELECT z.zimmernummer
+			FROM zimmer z
+			JOIN zimmer_rechnung zr
+			ON z.zimmernummer = zr.zimmernummer
+			WHERE zr.rechnungID = 
+				(SELECT r.rechnungID  FROM rechnung r 
+				INNER JOIN rechnung_gaeste rg ON rg.rechnungID = r.rechnungID		
+				INNER JOIN gaeste g ON g.personID = rg.personID
+				INNER JOIN person p ON p.personID = g.personID	
+				WHERE p.vorname = l_v_vorname_primary_in
+				AND p.nachname = l_v_nachname_primary_in
+				AND r.zeitstempel = TO_DATE(l_l_d_zeitstempel_in,  'YYYY-MM-DD'))
+			ORDER BY z.zimmernummer ASC;
+	-- Cursor for all invoices of one guest
+	CURSOR l_room_for_person_cur
+		IS
+			SELECT *
+			FROM rechnung r
+			JOIN rechnung_gaeste g
+			ON r.rechnungID = g.rechnungID
+			WHERE g.personID = (SELECT personID FROM person WHERE vorname = l_v_vorname_in AND nachname = l_v_nachname_in)
+			ORDER BY r.rechnungID ASC;
+			
+	l_i_help INT := 0;
+	l_i_help_1 INT := 0;
+	l_i_bettenanzahl INT := 0;
+	l_i_personID person.personID%TYPE := 0;
+	l_i_zimmernummer zimmer.zimmernummer%TYPE := 0;
+	l_i_rechnungID rechnung.rechnungID%TYPE; 
+	l_i_anzahl_naechte INTEGER;
+	x_no_checkin_found EXCEPTION;
+	x_person_id_not_exists EXCEPTION;
+	x_person_id_exists_multiple EXCEPTION;
+	x_no_bed_left EXCEPTION;
+  x_person_already_checked_in EXCEPTION;
+  errno INTEGER; 
+	errmsg char(200);
+BEGIN
+-- check if person exists
+	SELECT count(personid) INTO l_i_help FROM person WHERE vorname = l_v_vorname_in AND nachname = l_v_nachname_in; 
+	
+	IF l_i_help = 0
+	THEN
+		DBMS_OUTPUT.PUT_LINE('Person existiert nicht!');
+		RAISE x_person_id_not_exists;
+	ELSIF l_i_help = 1
+	THEN
+		SELECT personID INTO l_i_personID FROM person WHERE vorname = l_v_vorname_in AND nachname = l_v_nachname_in;
+
+		-- check if primary guest has checked in at given time
+		SELECT count(r.rechnungID) INTO l_i_help FROM rechnung r 
+		INNER JOIN rechnung_gaeste rg ON rg.rechnungID = r.rechnungID		
+		INNER JOIN gaeste g ON g.personID = rg.personID
+		INNER JOIN person p ON p.personID = g.personID	
+		WHERE p.vorname = l_v_vorname_primary_in
+		AND p.nachname = l_v_nachname_primary_in
+		AND r.zeitstempel = TO_DATE(l_l_d_zeitstempel_in,  'YYYY-MM-DD');
+		IF l_i_help <> 1
+		THEN
+			DBMS_OUTPUT.PUT_LINE('Angegebene Hauptbuchung wurde nicht gefunden!');
+			RAISE x_no_checkin_found;
+		ELSE	
+			SELECT r.rechnungID, r.anzahl_naechte INTO l_i_rechnungID, l_i_anzahl_naechte FROM rechnung r 
+			INNER JOIN rechnung_gaeste rg ON rg.rechnungID = r.rechnungID		
+			INNER JOIN gaeste g ON g.personID = rg.personID
+			INNER JOIN person p ON p.personID = g.personID	
+			WHERE p.vorname = l_v_vorname_primary_in
+			AND p.nachname = l_v_nachname_primary_in
+			AND r.zeitstempel = TO_DATE(l_l_d_zeitstempel_in,  'YYYY-MM-DD');
+			
+			-- check if there is a bed left in the booked room
+			FOR vResult IN l_rooms_for_invoice_cur
+			LOOP
+				SELECT count(rg.personID) INTO l_i_help FROM  rechnung_gaeste rg
+				INNER JOIN rechnung r ON rg.rechnungID = r.rechnungID		
+				INNER JOIN gaeste g ON g.personID = rg.personID
+				INNER JOIN zimmer_rechnung zr ON r.rechnungID = zr.rechnungID	
+				INNER JOIN zimmer z ON zr.zimmernummer  = z.zimmernummer
+				WHERE z.zimmernummer = vResult.zimmernummer
+				AND r.zeitstempel = TO_DATE(l_l_d_zeitstempel_in,  'YYYY-MM-DD');
+				
+				SELECT bettenzahl INTO l_i_help_1 FROM zimmer WHERE zimmernummer = vResult.zimmernummer;
+				-- check if checked in guests at a room is smaller than the amount of beds
+				DBMS_OUTPUT.PUT_LINE('Zimmer:' || vResult.zimmernummer);
+				DBMS_OUTPUT.PUT_LINE('Bettens des Zimmers:' || l_i_help_1);
+				
+				l_i_bettenanzahl := l_i_bettenanzahl + l_i_help_1;
+				DBMS_OUTPUT.PUT_LINE('Bettens Gesamt:' || l_i_bettenanzahl);
+				IF(l_i_help < l_i_bettenanzahl)
+				THEN
+					-- bed is found
+					l_i_zimmernummer := vResult.zimmernummer;
+					EXIT;
+				END IF;
+			END LOOP;
+			IF l_i_zimmernummer <> 0
+			THEN 
+				DBMS_OUTPUT.PUT_LINE('Zimmer wurde gefunden!');
+				
+				-- check if person is already added in guest list
+				SELECT count(personID) INTO l_i_help FROM gaeste WHERE personID = l_i_personID;
+				-- if not in guest list, add to guest list
+				IF l_i_help = 0
+				THEN
+				 INSERT INTO gaeste (personID)VALUES (l_i_personID);
+				END IF;
+				-- check if guest is aleady checked in for another room in the same timeframe				
+				FOR vResult IN l_room_for_person_cur
+				LOOP
+					DECLARE
+						l_d_to_date DATE; 
+						l_d_to_date_1 DATE; 
+						l_i_start INTEGER ;
+						l_i_end INTEGER;
+					BEGIN
+						l_i_start := 0;
+						l_i_end := 0;
+						l_i_help := 0;
+
+						SELECT TO_DATE(l_l_d_zeitstempel_in,  'YYYY-MM-DD') + l_i_anzahl_naechte INTO l_d_to_date FROM dual;					
+						SELECT count(1) INTO l_i_start 
+							FROM dual 
+							WHERE vResult.zeitstempel BETWEEN TO_DATE(l_l_d_zeitstempel_in,  'YYYY-MM-DD') AND l_d_to_date;		
+
+						SELECT vResult.zeitstempel + vResult.anzahl_naechte INTO l_d_to_date_1 FROM dual;					 
+						SELECT count(1) INTO l_i_end
+							FROM dual 
+							WHERE l_d_to_date_1 BETWEEN TO_DATE(l_l_d_zeitstempel_in,  'YYYY-MM-DD') AND l_d_to_date;	
+						l_i_help := l_i_start + l_i_end;
+						IF l_i_help <> 0
+						THEN
+						DBMS_OUTPUT.PUT_LINE('Person ist in demselben Zeitraum schon auf einem anderen Zimmer gebucht!');
+						RAISE x_person_already_checked_in;
+						EXIT;
+						END IF;
+					END;
+				END LOOP;
+				-- create entry in rechnung_gaeste
+				INSERT INTO rechnung_gaeste (rechnungID, personID) VALUES (l_i_rechnungID, l_i_personID );
+				COMMIT;
+			ELSE
+				DBMS_OUTPUT.PUT_LINE('Alle Betten der Zimmer der Rechnung sind belegt!');
+				RAISE x_no_bed_left;
+			END IF;
+		END IF;
+	ELSE 
+		DBMS_OUTPUT.PUT_LINE('Person existiert mehrmals!');
+		RAISE x_person_id_exists_multiple;
+	END IF;
+	
+EXCEPTION
+		WHEN x_person_id_not_exists 
+		THEN 
+			rollback;
+			errno := -20461;
+			errmsg := 'PersonID does not exists with given firstname and surname!';
+			raise_application_error(errno,errmsg);	
+		WHEN x_person_id_exists_multiple
+		THEN 
+			rollback;
+			errno := -20462;
+			errmsg := 'PersonID exists multiple times with given firstname and surname!';
+			raise_application_error(errno,errmsg);
+		WHEN x_no_bed_left
+		THEN 
+			rollback;
+			errno := -20466;
+			errmsg := 'There is no bed left!';
+			raise_application_error(errno,errmsg);
+		WHEN x_person_already_checked_in
+		THEN 
+			rollback;
+			errno := -20465;
+			errmsg := 'The person entered is already checked in at another room!';
+			raise_application_error(errno,errmsg);
+		WHEN 	x_no_checkin_found
+		THEN
+			ROLLBACK;
+			errno := -20467;
+			errmsg := 'The entered primary guest has not checked in yet!';
+			raise_application_error(errno,errmsg);
+END;
 /
- 
-EXEC sp_checkin ('Lina', 'Severovic','2020-01-22', 3, 'Suite','Vollpension ab 12');
+
+--SELECT * FROM gaeste_view;
+--SELECT * FROM person;
+--SELECT * FROM zimmer WHERE zimmernummer = 501; -- 6 Betten
+
+-- EXEC sp_checkin_add ('Ivy', 'Koller','2020-01-05', 'Lina', 'Severovic');
+
+/*********************************************************************
+/**
+/** Procedure sp_checkin_add_room
+/** Out: 
+/** In: l_d_zeitstempel_in - Ankunftdatum des Gastes.'YYYY-MM-DD'
+/** In: l_v_vorname_primary_in - Vorname zu dem das Zimmer hinzugebucht wird
+/** In: l_v_nachname_primary_in - Nachname zu dem das Zimmer hinzugebucht wird
+/** In: l_v_bezeichnung_in - Bezeichnung der gewünschten Zimmerkategorie.
+/** Developer: Alina Poljanc
+/** Description: The procedure adds a room to an existing invoice
+/**   					 In case the given name is not found in the person table,
+/** 						 there is no room left in the wished category  an
+/**							 exception is thrown. 
+/*********************************************************************/
+
+
+CREATE OR REPLACE PROCEDURE sp_checkin_add_room
+(l_d_zeitstempel_in IN VARCHAR,l_v_vorname_primary_in IN VARCHAR, l_v_nachname_primary_in IN VARCHAR, l_v_bezeichnung_in IN VARCHAR)
+AS
+-- Cursor for all rooms of a category
+	CURSOR l_room_for_category_cur
+		IS
+			SELECT zimmernummer
+			FROM zimmer
+			WHERE zimmerkategorieID = (SELECT zimmerkategorieID FROM zimmerkategorie WHERE bezeichnung = l_v_bezeichnung_in)
+			ORDER BY zimmernummer ASC;
+
+	l_i_help INT := 0;
+	l_i_anzahl_naechte INT; 	
+	l_i_personID person.personID%TYPE := 0;
+	l_i_zimmernummer zimmer.zimmernummer%TYPE := 0;
+	l_i_rechnungID rechnung.rechnungID%TYPE; 
+	l_v_result_room_status VARCHAR2(200); 
+	x_person_id_not_exists EXCEPTION;
+	x_person_id_exists_multiple EXCEPTION;
+	x_no_room_available EXCEPTION;
+  errno INTEGER; 
+	errmsg char(200);
+BEGIN
+-- check if person exists
+	SELECT count(personid) INTO l_i_help FROM person WHERE vorname = l_v_vorname_primary_in AND nachname = l_v_nachname_primary_in; 
+	
+	IF l_i_help = 0
+	THEN
+		DBMS_OUTPUT.PUT_LINE('Person existiert nicht!');
+		RAISE x_person_id_not_exists;
+	ELSIF l_i_help = 1
+	THEN
+		-- get personID  of primary guest
+		SELECT personID INTO l_i_personID FROM person WHERE vorname = l_v_vorname_primary_in AND nachname = l_v_nachname_primary_in;
+		
+		-- get rechnungID of invoice and anzahl_naechte 
+		SELECT count(r.rechnungID) INTO l_i_help FROM rechnung r
+		INNER JOIN rechnung_gaeste rg ON rg.rechnungID = r.rechnungID
+		INNER JOIN gaeste g ON g.personID = rg.personID
+		INNER JOIN person p ON p.personID = g.personID
+		WHERE p.personID = l_i_personID
+		AND r.zeitstempel = TO_DATE(l_d_zeitstempel_in,  'YYYY-MM-DD');
+		
+		IF l_i_help <> 1
+		THEN
+			DBMS_OUTPUT.PUT_LINE('Person hat zu dem angegebenen Datum noch nichts gebucht!');
+			RAISE x_no_room_available;
+		END IF;
+		
+		SELECT r.rechnungID, r.anzahl_naechte INTO l_i_rechnungID, l_i_anzahl_naechte FROM rechnung r
+		INNER JOIN rechnung_gaeste rg ON rg.rechnungID = r.rechnungID
+		INNER JOIN gaeste g ON g.personID = rg.personID
+		INNER JOIN person p ON p.personID = g.personID
+		WHERE p.personID = l_i_personID
+		AND r.zeitstempel = TO_DATE(l_d_zeitstempel_in,  'YYYY-MM-DD');
+		
+		-- check if zimmerkategorie is valid 
+		SELECT count(zimmerkategorieID) INTO l_i_help FROM zimmerkategorie WHERE bezeichnung = l_v_bezeichnung_in;
+		IF l_i_help <> 1
+		THEN
+			DBMS_OUTPUT.PUT_LINE('Zimmerkategorie konnte nicht gefunden werden!');
+			RAISE x_no_room_available;
+		ELSE	
+			-- find room in given category
+			FOR vResult IN l_room_for_category_cur
+			LOOP
+				BEGIN
+					sp_room_status(vResult.zimmernummer,l_v_result_room_status);
+				END;
+				IF l_v_result_room_status IS NULL
+				THEN 
+					-- not yet booked room is found
+					l_i_zimmernummer := vResult.zimmernummer;
+					EXIT;
+				ELSE
+					-- booked roomed is checked if the timeframes are overlapping
+					DECLARE
+						l_d_to_date DATE; 
+					BEGIN 
+						SELECT TO_DATE(l_d_zeitstempel_in,  'YYYY-MM-DD') + l_i_anzahl_naechte INTO l_d_to_date FROM dual;
+						DBMS_OUTPUT.PUT_LINE(l_d_zeitstempel_in);
+						DBMS_OUTPUT.PUT_LINE(l_v_result_room_status);
+						SELECT count(1) INTO l_i_help
+						FROM dual 
+						WHERE TO_DATE(l_v_result_room_status, 'YYYY-MM-DD HH24:MI') BETWEEN TO_DATE(l_d_zeitstempel_in,  'YYYY-MM-DD') AND l_d_to_date;			
+					END;
+					IF l_i_help = 1
+					THEN
+					-- room is available for the timeframe
+						l_i_zimmernummer := vResult.zimmernummer;
+						EXIT;
+					END IF;
+				END IF;
+			END LOOP;
+			IF l_i_zimmernummer <> 0
+			THEN 
+				DBMS_OUTPUT.PUT_LINE('Zimmer wurde gefunden!');
+				-- create entry in zimmer_rechnung
+					INSERT INTO zimmer_rechnung (zimmernummer, rechnungID) VALUES (l_i_zimmernummer,l_i_rechnungID);	
+					COMMIT;		
+			ELSE
+				DBMS_OUTPUT.PUT_LINE('Kein Zimmer in der angegebenenen Kategorie verfuegbar!');
+				RAISE x_no_room_available;
+			END IF;
+		END IF;
+	ELSE 
+		DBMS_OUTPUT.PUT_LINE('Person existiert mehrmals!');
+		RAISE x_person_id_exists_multiple;
+	END IF;
+	
+EXCEPTION
+		WHEN x_person_id_not_exists 
+		THEN 
+			rollback;
+			errno := -20461;
+			errmsg := 'PersonID does not exists with given firstname and surname!';
+			raise_application_error(errno,errmsg);	
+		WHEN x_person_id_exists_multiple
+		THEN 
+			rollback;
+			errno := -20462;
+			errmsg := 'PersonID exists multiple times with given firstname and surname!';
+			raise_application_error(errno,errmsg);
+		WHEN x_no_room_available
+		THEN 
+			rollback;
+			errno := -20463;
+			errmsg := 'There is no room for the given category or at the given timeframe available!';
+			raise_application_error(errno,errmsg);
+END;
 /
-*****************************************************************************************/
 
 
+--SELECT * FROM gaeste_view;
+--SELECT * FROM person;
+--SELECT * FROM zimmer WHERE zimmernummer = 504; -- 6 Betten
 
+--EXEC sp_checkin_add ('Ivy', 'Koller','2020-01-05', 'Lina', 'Severovic');
+--EXEC sp_checkin_add_room ('2020-01-05', 'Lina', 'Severovic', 'Suite');
+
+
+/
--- create_views.sql
+++ create_views.sql
@@ -104,3 +104,66 @@ FROM rechnung
          JOIN pensionsform_tageskarte USING (pensionsform_tageskarteID);
 /
 -- SELECT * FROM rechnung_view;
+
+/*********************************************************************
+/**
+/** Table View: gaeste_view
+/** Developer: Alina Poljanc
+/** Description: This view shows the for the guests relevant data. 
+/**
+/*********************************************************************/
+CREATE OR REPLACE VIEW gaeste_view
+AS
+	SELECT p.vorname AS vorname, p.nachname AS nachname, z.zimmernummer AS zimmernummer, zk.bezeichnung AS Zimmerkategorie, pt.bezeichnung AS pensionsform, r.zeitstempel AS von, (r.zeitstempel + r.anzahl_naechte) AS bis FROM person p
+	INNER JOIN gaeste g ON p.personID = g.personID	
+	INNER JOIN rechnung_gaeste rg ON g.personID = rg.personID
+	INNER JOIN rechnung r ON rg.rechnungID = r.rechnungID
+	INNER JOIN zimmer_rechnung zr ON zr.rechnungID = r.rechnungID
+	INNER JOIN zimmer z ON z.zimmernummer  = zr.zimmernummer
+	LEFT JOIN zimmerkategorie zk ON zk.zimmerkategorieID  = z.zimmerkategorieID
+	LEFT JOIN pensionsform_tageskarte pt ON pt.pensionsform_tageskarteID  = r.pensionsform_tageskarteID;
+/
+--select * from gaeste_view;
+
+/*********************************************************************
+/**
+/** Type  + Function : t_varchar2_tab + tab_to_string 
+/** Developer: ORACLE 
+/** Description: Enables to create the gaeste_view grouped in Oracle 10g 
+/**								--> see https://oracle-base.com/articles/misc/string-aggregation-techniques#collect
+/*********************************************************************/
+
+
+CREATE OR REPLACE TYPE t_varchar2_tab AS TABLE OF VARCHAR2(4000);
+/
+
+CREATE OR REPLACE FUNCTION tab_to_string (p_varchar2_tab  IN  t_varchar2_tab,
+                                          p_delimiter     IN  VARCHAR2 DEFAULT ',') RETURN VARCHAR2 IS
+  l_string     VARCHAR2(32767);
+BEGIN
+  FOR i IN p_varchar2_tab.FIRST .. p_varchar2_tab.LAST LOOP
+    IF i != p_varchar2_tab.FIRST THEN
+      l_string := l_string || p_delimiter;
+    END IF;
+    l_string := l_string || p_varchar2_tab(i);
+  END LOOP;
+  RETURN l_string;
+END tab_to_string;
+/
+/*********************************************************************
+/**
+/** Table View: gaeste_view_grouped
+/** Developer: Alina Poljanc
+/** Description: This view shows the for the guests relevant data 
+/**              grouped by booking and name 
+/**
+/*********************************************************************/
+
+CREATE OR REPLACE VIEW gaeste_view_grouped
+AS
+	SELECT  nachname,vorname, von, bis, max(pensionsform) as pensionform,tab_to_string( CAST(COLLECT (TO_CHAR(zimmernummer)) AS t_varchar2_tab))AS zimmernummern
+	FROM gaeste_view
+	GROUP BY nachname ,vorname, von, bis
+	ORDER BY nachname ,vorname, von, bis;  
+/
+--select * from gaeste_view_grouped;
